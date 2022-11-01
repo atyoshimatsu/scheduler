3267ac9d8d2d64c31675dcfcf3fcdef64ad2de24
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-01 00:22:30 -0700

    Modify DayListItem component to change messages depending on spots props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,22 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayListItemClass = classNames(
+    'day-list__item',
+    { 'day-list__item--selected': props.selected },
+    { 'day-list__item--full': props.spot === 0 }
+);
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} className={dayListItemClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">
+        {props.spots > 1 && `${props.spots} spots remaining`}
+        {props.spots === 1 && `1 spot remaining`}
+        {props.spots === 0 && `no spots remaining`}
+      </h3>
     </li>
   );
 }
