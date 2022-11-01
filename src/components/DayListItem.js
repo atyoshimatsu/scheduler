f3219d71b1bcfa5626bce5346e61333c9fa4bd48
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-01 00:26:33 -0700

    Add indent

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,7 +7,7 @@ export default function DayListItem(props) {
     'day-list__item',
     { 'day-list__item--selected': props.selected },
     { 'day-list__item--full': props.spot === 0 }
-);
+  );
 
   return (
     <li onClick={() => props.setDay(props.name)} className={dayListItemClass}>
