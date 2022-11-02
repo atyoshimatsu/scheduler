d3d960bff6641eab89028867d3c03ea0246c71af
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-02 16:42:28 -0700

    Create Header component and story

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,7 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
 
 const days = [
   {
@@ -135,3 +136,4 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time={"12pm"}/>)
+  .add("Header", () => <Header time={"12pm"} />)
