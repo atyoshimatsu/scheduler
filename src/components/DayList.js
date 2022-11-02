d03825b2b1b2fbf8cca189b0653ab59651ffa025
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-02 15:31:49 -0700

    Change props name in the DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,12 +2,13 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const dayList = props.days.map(d => <DayListItem
+  const { days, value, onChange } = props;
+  const dayList = days.map(d => <DayListItem
       key={d.id}
       name={d.name}
       spots={d.spots}
-      selected={d.name === d.day}
-      setDay={props.setDay}
+      selected={d.name === value}
+      setDay={onChange}
     />
   );
   return <ul>{dayList}</ul>;
