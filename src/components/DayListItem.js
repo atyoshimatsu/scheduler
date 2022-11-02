d05d2ac227582a33060f66d8b0a1bfbc48de0841
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-02 15:32:43 -0700

    Use destructuring assignment

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,19 +3,20 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const { name, selected, spots, setDay } = props;
   const dayListItemClass = classNames(
     'day-list__item',
-    { 'day-list__item--selected': props.selected },
-    { 'day-list__item--full': props.spot === 0 }
+    { 'day-list__item--selected': selected },
+    { 'day-list__item--full': spots === 0 }
   );
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayListItemClass}>
-      <h2 className="text--regular">{props.name}</h2>
+    <li onClick={() => setDay(name)} className={dayListItemClass}>
+      <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">
-        {props.spots > 1 && `${props.spots} spots remaining`}
-        {props.spots === 1 && `1 spot remaining`}
-        {props.spots === 0 && `no spots remaining`}
+        {spots > 1 && `${spots} spots remaining`}
+        {spots === 1 && `1 spot remaining`}
+        {spots === 0 && `no spots remaining`}
       </h3>
     </li>
   );
