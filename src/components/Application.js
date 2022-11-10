e92b54cd26c54d48b163743e7aaf8a22c18aef4c
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-10 09:51:56 -0800

    Add day and days to state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
@@ -45,19 +45,27 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
   useEffect(() => {
     axios.get('http://localhost:8001/api/days')
       .then(res => {
-        setDays([...res.data]);
-      })
+        setDays(res.data);
+      });
   }, []);
-  console.log(days);
+
+
   const schedule = Object.values(appointments).map(appointment => {
     return <Appointment key={appointment.id} {...appointment} />
   });
 
+  console.log('state', state);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -69,8 +77,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
       </nav>
