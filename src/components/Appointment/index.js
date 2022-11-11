a072359f6dc17e5f5c51b41463c82939c179548a
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-11 10:50:59 -0800

    Add cutom hook to Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,35 @@ import "components/Appointment/styles.scss";
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+import useVisualMode from 'hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 function Appointment(props) {
+  const { mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   const { id, time, interview } = props;
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview
-        ? <Show
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW &&
+        <Show
             student={interview.student}
             interviewer={interview.interviewer}
-          />
-        : <Empty />
+        />
+      }
+      {mode === CREATE &&
+        <Form
+          interviewers={[]}
+          onSave={() => console.log('Clicked Save!')}
+          onCancel={() => back(EMPTY)}
+        />
       }
     </article>
   );
