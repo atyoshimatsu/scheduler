5f5248bd18bdb937e5c2f1dd1d81803cb0752f7c
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-10 22:57:53 -0800

    Modify Application.js to fetch data using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,68 +4,32 @@ import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
 import axios from "axios";
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: 'Monday',
     days: [],
+    appointments: {},
   });
-
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
 
-  useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-      .then(res => {
-        setDays(res.data);
-      });
+  useEffect( () => {
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+
+    });
   }, []);
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const schedule = Object.values(appointments).map(appointment => {
+  const schedule = dailyAppointments.map(appointment => {
     return <Appointment key={appointment.id} {...appointment} />
   });
 
-  console.log('state', state);
   return (
     <main className="layout">
       <section className="sidebar">
