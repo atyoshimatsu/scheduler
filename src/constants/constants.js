e71f7653cfcb956446cc92a6705ff22def03dc58
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-16 22:12:41 -0800

    Create constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+export const EMPTY = "EMPTY";
+export const SHOW = "SHOW";
+export const CREATE = "CREATE";
+export const EDIT = "EDIT";
+export const CONFIRM = "CONFIRM";
+export const SAVING = "SAVING";
+export const DELETING = "DELETING";
+export const ERROR_SAVE = "ERROR_SAVE";
+export const ERROR_DELETE = "ERROR_DELETE";
