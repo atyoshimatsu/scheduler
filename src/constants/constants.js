dc46cb6d236daaf4ddbd2eb66a0cf83e3beca169
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-18 15:17:38 -0800

    Add reducers action to constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -7,3 +7,7 @@ export const SAVING = "SAVING";
 export const DELETING = "DELETING";
 export const ERROR_SAVE = "ERROR_SAVE";
 export const ERROR_DELETE = "ERROR_DELETE";
+
+export const SET_DAY = "SET_DAY";
+export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+export const SET_INTERVIEW = "SET_INTERVIEW";
