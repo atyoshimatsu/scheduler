20e695b0e85447aa0b1107dd5c6f60c0ab317e36
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-26 18:37:46 -0800

    Create another integration test case

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+  getByText,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+  prettyDOM,
+} from "@testing-library/react";
 import Application from "components/Application";
 
 afterEach(cleanup);
@@ -9,4 +19,19 @@ it("defaults to Monday and changes the schedule when a new day is selected", asy
   await waitForElement(() => getByText("Monday"));
   fireEvent.click(getByText("Tuesday"));
   expect(getByText("Leopold Silvers")).toBeInTheDocument();
+});
+
+it.only("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  const { container } = render(<Application />);
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointment = getAllByTestId(container, "appointment")[0];
+  fireEvent.click(getByAltText(appointment, "Add"));
+  fireEvent.change(getByPlaceholderText(appointment, /Enter Student Name/i), {
+    target: { value: "Lydia Miller-Jones" },
+  });
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  fireEvent.click(getByText(appointment, "Save"));
+  expect(getByText(appointment, "Saving")).toBeInTheDocument();
+  console.log(prettyDOM(appointment));
 });
